8f10ccaf144252cfd6588f46ac145468ee6cdfe2
PeterTakahashi <twitter333555@gmail.com>
2025-05-16 15:57:46 +0800

    fix not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -3,7 +3,15 @@ import type { FC } from "react";
 export const NotFoundPage: FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">NotFoundPage</h1>
+      <h1 className="text-4xl font-bold">NotFound</h1>
+      <p className="text-lg text-gray-700 mb-4">404</p>
+      <p className="text-lg text-gray-700">This page does not exist.</p>
+      <p className="text-lg text-gray-700">
+        Go back to{" "}
+        <a href="/" className="font-semibold text-teal-600 hover:text-teal-500">
+          Home
+        </a>
+      </p>
     </div>
   );
 };
