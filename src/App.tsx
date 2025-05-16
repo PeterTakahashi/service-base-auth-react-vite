47ad8817b4aac88dbae6b157b52ae7ab33c2df54
PeterTakahashi <twitter333555@gmail.com>
2025-05-16 15:26:47 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
-export default function Example() {
+function App() {
   return (
-    <>
-      <button
-        type="button"
-        className="rounded-sm bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Button text
-      </button>
-      <button
-        type="button"
-        className="rounded-sm bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Button text
-      </button>
-      <button
-        type="button"
-        className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Button text
-      </button>
-      <button
-        type="button"
-        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Button text
-      </button>
-      <button
-        type="button"
-        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Button text
-      </button>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   );
 }
+
+export default App;
