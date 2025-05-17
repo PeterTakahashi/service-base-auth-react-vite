3b459e0a4d2e17320df7bcfae6340a56e63374d5
PeterTakahashi <twitter333555@gmail.com>
2025-05-17 13:20:24 +0800

    検証メール送信ボタンの追加

--- src/pages/NotVerifiedPage/index.tsx
+++ src/pages/NotVerifiedPage/index.tsx
@@ -23,6 +23,17 @@ export const NotVerifiedPage: FC = () => {
         Verification email sent. Please check your inbox.
       </p>
       {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
+      <div className="mt-16 text-center text-sm/6 text-gray-500">
+        <div>
+          Did you receive the email?{" "}
+          <a
+            href=""
+            className="font-semibold text-teal-600 hover:text-teal-500"
+          >
+            Resend verification email
+          </a>
+        </div>
+      </div>
       <button
         onClick={logout}
         className="mt-4 px-4 py-2 bg-teal-600 text-white rounded hover:bg-teal-700"
