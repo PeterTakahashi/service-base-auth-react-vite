086400bba694fdef12fc0f31fef12fb7951223d9
PeterTakahashi <twitter333555@gmail.com>
2025-05-19 13:09:48 +0800

    add login

--- src/features/hooks/swr/fetcher/user/useUser.ts
+++ src/features/hooks/swr/fetcher/user/useUser.ts
@@ -1,6 +1,7 @@
 import useSWR from "swr";
 import { fetcher } from "@/features/hooks/swr/fetcher/fetcher";
 import type { UserRead } from "@/types/api/user";
+import { useNavigate } from "react-router-dom";
 
 export function useUser() {
   const {
@@ -9,6 +10,15 @@ export function useUser() {
     isLoading,
     mutate,
   } = useSWR<UserRead>("/users/me", fetcher);
+  const navigate = useNavigate();
+
+  if (!isLoading && error) {
+    if (error.status === 401) {
+      navigate("/signin", {
+        state: { errorMessage: "You must be logged in to access this page." },
+      });
+    }
+  }
 
   return {
     user,
